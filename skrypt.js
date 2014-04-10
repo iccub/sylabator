e0f9d1972b0b5907073e6a1f957e2d56b1a4c783
iccuB <mail@bucci.pl>
2014-04-11 00:36:43 +0200

    v0.2

--- skrypt.js
+++ skrypt.js
@@ -12,9 +12,26 @@ function countSyllabes()
 	for (var i = 0; i < arrayOfLines.length; i++) {
 		var row = table.insertRow(i);
 		var cell1 = row.insertCell(0);
+
+
+
 		cell1.innerHTML = countLineSyllabes(arrayOfLines[i]);
+		setCounterColor(cell1);
+
 	}
 
+}
+
+function setCounterColor(cell) {
+	var number = 0;
+	number = cell.innerHTML;
+	var diffBetweenTargetCount = Math.abs(document.getElementById('slider').value - number);
+	
+	var g = 255 - diffBetweenTargetCount * 40;
+	var r = 0 + diffBetweenTargetCount * 40;
+	var color = "rgb(" + r +"," + g +" ,100)";
+
+	cell.style.color = color; 
 }
 
 function countLineSyllabes (lineOfText) {
@@ -35,30 +52,32 @@ function countLineSyllabes (lineOfText) {
 			*
 			*/
 
-			
 			var count_of_i = word[i].toLowerCase().split(/[i]/g);
 			if (count_of_i != null && count_of_i.length > 1) {
 				for (var j = 1; j < count_of_i.length; j++) {
 					var nextLetter = count_of_i[j][0];
 					if (nextLetter != null) {
-						if(nextLetter.match(/[aeiouąęó]/gi) != null) {
-							console.log("Jest zmiekczenie");
-							temp--;
-						}
+						if(nextLetter.match(/[aeiouąęó]/gi) != null) 
+							temp--;						
 					};
-					
-					
 				};
 			};
-
-
-			
-			
-
-			
-
 		}
 	}
 
-	return temp;
+	if (temp != 0) 
+		return temp;
+	else
+		return "-";
+}
+
+function updateSyllabeCount() {
+	document.getElementById('syllabeCounter').innerHTML= document.getElementById('slider').value; 
+}
+
+function save()
+{
+	var content = document.getElementById("notes").value;
+	var blob = new Blob([content], {type: "text/plain;charset=utf-8"});
+	saveAs(blob, "tekst.txt");
 }
